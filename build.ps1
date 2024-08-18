98a7d8b052a7725b6bcf6d67900d9aba539c1594
Eisai <12467320+Eisaichen@users.noreply.github.com>
2024-08-18 06:01:58 -0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -2,7 +2,11 @@
 Expand-Archive -Path .\ollama-windows-amd64.zip -DestinationPath .\build\ollama-windows-amd64
 
 docker pull mcr.microsoft.com/windows/servercore:ltsc2022
-docker build --isolation hyperv --no-cache -t eisai/ollama:latest -t eisai/ollama:$env:GH_CI_TAG .\build
+if ($env:GH_CI_PUSH -eq "true") {
+    docker build --isolation hyperv --no-cache -t eisai/ollama:latest -t eisai/ollama:$env:GH_CI_TAG .\build
+} else {
+    docker build --isolation hyperv --no-cache -t eisai/ollama:$env:GH_CI_TAG .\build
+}
 
 if ($env:GH_CI_PUSH -eq "true") {
     docker push eisai/ollama -a
